675b330780cd2c10b58e4b0c003a342518b7fcc8
ericbarber <eric.a.barber@gmail.com>
2025-04-03 12:56:41 -0700

    Minor Commit:
    Changes to be committed:
    	modified:   README.md
    	- Update to include tamplate space for 'commit_hash'
    	modified:   esource_pipeline/bd_utils/db_init.py
    	- Update db_init to include version log, load log and schema

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # esource_pipeline
 Demo project for data pipeline
+
+
+# Pip install commands
+Install commands
+Specific Commit
+
+pip install --upgrade https://github.com/ericbarber/esource_pipeline/archive/.zip
+Upgrade to prods most recent build
+
+pip install --upgrade https://github.com/ericbarber/esource_pipeline/archive/refs/heads/main.zip
+Install prods most recent build
+
+pip install https://github.com/ericbarber/esource_pipeline/archive/refs/heads/main.zip
+Install from version
+
+pip install --upgrade https://github.com/ericbarber/esource_pipeline/archive/refs/tags/v1.0.0.zip
+Check what is installed
+
+pip freeze | grep esource
--- esource_pipeline/bd_utils/db_init.py
+++ esource_pipeline/bd_utils/db_init.py
@@ -1,16 +1,39 @@
 from pyspark.sql import SparkSession
 spark = SparkSession.builder.getOrCreate()
 
-spark.sql("CREATE SCHEMA IF NOT EXISTS esource")
-spark.catalog.setCurrentDatabase("esource")
-
 from pyspark.sql.types import StringType, LongType, TimestampType
-version_tracking_table_name = "esource.version_tracking"
-if not spark.catalog.tableExists(version_tracking_table_name):
-    tracking_schema = StructType([
-        StructField("table_name", StringType(), False),
-        StructField("last_processed_version", LongType(), False),
-        StructField("last_updated", TimestampType(), True)
-    ])
-    tracking_df = spark.createDataFrame([], schema=tracking_schema)
-    tracking_df.write.format("delta").mode("overwrite").saveAsTable(version_tracking_table_name)
+
+def db_init():
+    # Initialize 
+    esource_schema = 'esource'
+    spark.sql(f"CREATE SCHEMA IF NOT EXISTS {esource_schema}")
+    spark.catalog.setCurrentDatabase("esource")
+
+    version_tracking_table = f"{esource_schema}.version_tracking"
+    if not spark.catalog.tableExists(version_tracking_table_name):
+        tracking_schema = StructType([
+            StructField("table_name", StringType(), False),
+            StructField("last_processed_version", LongType(), False),
+            StructField("last_updated", TimestampType(), True)
+        ])
+        tracking_df = spark.createDataFrame([], schema=tracking_schema)
+        tracking_df.write.format("delta").mode("overwrite").saveAsTable(version_tracking_table)
+
+    load_log_table = f"{esource_schema}.load_log"
+    if not spark.catalog.tableExists(load_log_table):
+        load_log_schema = StructType([
+            StructField("file_name", StringType(), False),
+            StructField("file_hash", LongType(), False),
+            StructField("load_time", TimestampType(), True)
+        ])
+        load_lof_df = spark.createDataFrame([], schema=load_log_schema) \
+        load_lof_df.write.format("delta").mode("overwrite").saveAsTable(load_log_table)
+        
+    print(
+        "DATABASE INITIALIZED",
+        f"COMPLETE CREATE SCHEMA: {esource_schema}",
+        f"COMPLETE CREATE TABLE: {load_log_table}"
+        f"COMPLETE CREATE TABLE: {version_tracking_table}"
+    )
+
+    return True
